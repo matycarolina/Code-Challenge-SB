2d710787d82d9dc33a45c941baecd1a70aa353d7
Marialejandra Contreras <34869971+matycarolina@users.noreply.github.com>
2022-06-15 17:54:21 -0500

    feat: :sparkles: Build CLI with TypeScript

--- index.js
+++ index.js
@@ -1,21 +1,2 @@
-// import the readline module for work with stdin, or stdout.
-const readline = require("readline");
-
-// create a readline object to work with the stream.
-// pass the stdin, or stdout in the current process.
-const prompts = readline.createInterface(process.stdin, process.stdout);
-
-// create a question or there handler.
-prompts.question("Enter Learning Resources Name : ", (response) => {
-  // check the response.
-  if (response.toLocaleLowerCase() == "gfg") {
-    console.log("You are a part of the very huge learning community.");
-  } else {
-    console.log(
-      "Have a look at Geeksforgeeks, they solve many of your technical doubts."
-    );
-  }
-
-  // after the all work is done want to terminate this process.
-  process.exit();
-});
+"use strict";
+console.log("Hello from Typescript");
